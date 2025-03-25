495d24d6b34f0e5ed7a06ab6e48428baf9df766c
njordulv <njordulv@gmail.com>
2025-03-25 11:23:58 +0200

    update: product schema with new timestamp & disabled unnecessary fields

--- models/Product.ts
+++ models/Product.ts
@@ -1,20 +1,23 @@
 import mongoose from 'mongoose'
 
-const ProductSchema = new mongoose.Schema({
-  title: String,
-  price: String,
-  oldPrice: String,
-  discount: String,
-  isGenius: Boolean,
-  stock: String,
-  stockOut: String,
-  stockLimited: String,
-  toOrder: String,
-  imageUrl: String,
-  link: String,
-  category: String,
-  timestamp: { type: Date, default: Date.now },
-})
+const ProductSchema = new mongoose.Schema(
+  {
+    title: String,
+    price: String,
+    oldPrice: String,
+    discount: String,
+    isGenius: Boolean,
+    stock: String,
+    stockOut: String,
+    stockLimited: String,
+    toOrder: String,
+    imageUrl: String,
+    link: String,
+    category: String,
+    timestamp: { type: Number, default: () => Date.now() },
+  },
+  { versionKey: false } // Disable versionKey __v field
+)
 
 export default mongoose.models.Product ||
   mongoose.model('Product', ProductSchema)
