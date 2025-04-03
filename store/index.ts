f83257d8f42dbe08df00644257e5c51bf965e8e4
njordulv <njordulv@gmail.com>
2025-04-03 22:21:49 +0300

    feat: grid view local storage from zustand persist

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { StoreProps } from '@/interfaces/ui'
 
-export const useStore = create<StoreProps>((set) => ({
-  currentPage: 1,
-  setCurrentPage: (page: number) => set({ currentPage: page }),
-  totalPages: 1,
-  setTotalPages: (page: number) => set({ totalPages: page }),
-  tagProducts: 0,
-  setTagProducts: (products: number) => set({ tagProducts: products }),
-  isGridView: false,
-  setIsGridView: (value: boolean) => set({ isGridView: value }),
-}))
+export const useStore = create<StoreProps>()(
+  persist(
+    (set) => ({
+      currentPage: 1,
+      setCurrentPage: (page: number) => set({ currentPage: page }),
+      totalPages: 1,
+      setTotalPages: (page: number) => set({ totalPages: page }),
+      tagProducts: 0,
+      setTagProducts: (products: number) => set({ tagProducts: products }),
+      isGridView: false,
+      setIsGridView: (value: boolean) => set({ isGridView: value }),
+    }),
+    {
+      name: 'view-mode-storage',
+      partialize: (state) => ({ isGridView: state.isGridView }),
+    }
+  )
+)
