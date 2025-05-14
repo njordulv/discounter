b084321fbfa2e5bae3e4b00ee06e7922d71b207d
njordulv <njordulv@gmail.com>
2025-05-14 21:22:42 +0300

    update: product page layout, ssr fix

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -6,14 +6,16 @@ import { TbExternalLink } from 'react-icons/tb'
 import { connectDB } from '@/lib/mongo'
 import Product from '@/models/Product'
 import { FallbackImage } from '@/components/ui/FallbackImage'
+import { LinkToShop } from '@/components/emag/card/Details'
 import type { ScrapeProps } from '@/interfaces/emag'
 import config from '@/config'
 
 interface ProductPageProps {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }
 
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductPage(props: ProductPageProps) {
+  const params = await props.params
   await connectDB()
 
   const product = await Product.findById(params.id).lean<ScrapeProps>()
@@ -21,54 +23,54 @@ export default async function ProductPage({ params }: ProductPageProps) {
   if (!product) return notFound()
 
   return (
-    <div className="max-w-5xl mx-auto">
-      <div className="grid grid-cols-[2fr_3fr] gap-4">
-        {product.imageUrl ? (
-          <Image
-            src={product.imageUrl}
-            alt={product.title}
-            width={384}
-            height={384}
-            className="w-96 h-96 object-cover aspect-square rounded-lg bg-background border"
-            priority={false}
-            placeholder="blur"
-            blurDataURL={config.imageBase64}
-          />
-        ) : (
-          <FallbackImage />
-        )}
-        <div className="flex flex-col gap-2 rounded-xl p-6 border bg-card text-sm text-muted-foreground shadow w-full">
-          <h1 className="text-2xl font-semibold text-card-foreground mb-4">
-            {product.title}
-          </h1>
-          <p className="text-lg">
-            Цена: <span className="font-semibold">{product.price} лв</span>
-          </p>
-          {product.oldPrice && (
-            <p className="text-sm text-muted-foreground">
-              Старая цена:{' '}
-              <span className="line-through">{product.oldPrice} лв</span>
-            </p>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-[auto_1fr] gap-4">
+        <div>
+          {product.imageUrl ? (
+            <Image
+              src={product.imageUrl}
+              alt={product.title}
+              width={384}
+              height={384}
+              className="object-cover rounded-lg bg-background w-full h-full sm:w-auto sm:h-auto sm:min-w-96"
+              priority={false}
+              placeholder="blur"
+              blurDataURL={config.imageBase64}
+            />
+          ) : (
+            <FallbackImage />
           )}
-          {product.discount > 0 && (
-            <p className="text-green-600 font-semibold">
-              Скидка: {product.discount}%
+        </div>
+        <div className="w-full flex flex-col justify-between gap-4 rounded-xl sm:p-6 p-3 border bg-card text-sm text-muted-foreground shadow">
+          <div>
+            <h1 className="md:text-2xl text-xl font-semibold text-card-foreground mb-4">
+              {product.title}
+            </h1>
+            <p>
+              <span className="text-3xl font-semibold bg-gradient inline !bg-clip-text text-transparent">
+                {product.price} лв
+              </span>
             </p>
-          )}
-          <p>
-            Avaible at: <a href={config.emag.url}>{product.store}</a>
-          </p>
+            {product.discount > 0 && (
+              <p className="text-accent font-semibold">
+                Скидка: {product.discount}%
+              </p>
+            )}
+            <p>
+              <LinkToShop store={product.store} />
+            </p>
+          </div>
           <a
             href={product.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-full"
+            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors border shadow-sm border-input h-9 px-4 py-2 modern"
           >
             <span>Перейти в магазин</span>
             <TbExternalLink size={18} />
           </a>
         </div>
       </div>
-    </div>
+    </>
   )
 }
