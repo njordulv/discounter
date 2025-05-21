295f494f17031f6e771de133bf6d44c3ab23b4fb
njordulv <njordulv@gmail.com>
2025-05-21 19:14:35 +0300

    update: mobile menu btm position & footer text size

--- components/Content.tsx
+++ components/Content.tsx
@@ -7,8 +7,8 @@ export const Content = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="wrapper">
       <Aside />
-      <MobileSidebar />
       <main className="relative w-full overflow-auto flex-1 flex flex-col min-h-[calc(100vh-3rem)]">
+        <MobileSidebar />
         <Header />
         <section className="max-w-5xl w-full h-auto mx-auto flex flex-col sm:py-16 py-2 px-2 gap-2">
           {children}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { getYear } from '@/utils'
 
 export default function Footer() {
   return (
-    <footer className="p-3 bg-card text-card-foreground flex justify-center gap-6">
+    <footer className="p-3 text-sm bg-card text-card-foreground flex justify-center text-center gap-6">
       <p>{`© Copyright ${getYear()} by Njordr. All rights reversed.`}</p>
     </footer>
   )
