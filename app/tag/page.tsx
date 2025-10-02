0def058e2ee16f77cbfdff7a83292eb04597ff16
njordulv <njordulv@gmail.com>
2025-10-02 19:13:33 +0300

    upd: Discover component

--- app/tag/page.tsx
+++ app/tag/page.tsx
@@ -3,8 +3,10 @@ import { Discover, CatsList } from '@/components/tags'
 export default function Page() {
   return (
     <>
-      <h1 className="w-full sm:text-4xl text-2xl text-left font-medium text-foreground">All Categories</h1>
-      <Discover slug="" />
+      <h1 className="w-full sm:text-4xl text-2xl text-left font-medium text-foreground">
+        All Categories
+      </h1>
+      <Discover slug="" showCount={false} />
       <CatsList />
     </>
   )
--- components/tags/Discover.tsx
+++ components/tags/Discover.tsx
@@ -4,20 +4,32 @@ import { slugToName, getMonth, getYear } from '@/utils'
 import { useStore } from '@/store'
 import config from '@/config'
 
-export const Discover = ({ slug }: { slug: string }) => {
+export const Discover = ({
+  slug,
+  showCount = true,
+}: {
+  slug: string
+  showCount?: boolean
+}) => {
   const { tagProducts } = useStore()
 
   return (
-    <p className="w-full flex sm:flex-row flex-col gap-1 mb-4 text-foreground">
-      {tagProducts > 0 && (
+    <p className="w-full flex gap-1 mb-4 text-foreground dark:text-shadow-2xs">
+      {showCount ? (
+        <>
+          <span>
+            {tagProducts || <span className="animate-pulse w-4">0</span>} deals
+            in {slugToName(slug) || ''} ·
+          </span>
+          <span>
+            {config.site.locale} · {getMonth()} {getYear()}
+          </span>
+        </>
+      ) : (
         <span>
-          <b>{tagProducts}</b> {tagProducts === 1 ? 'deal and offer' : 'deals and offers'}.
+          {`All deals in ${config.site.locale} · ${getMonth()} ${getYear()}.`}
         </span>
       )}
-
-      <span>{`Discover ${slugToName(slug) || 'all'} offers and sales in ${
-        config.site.locale
-      }, ${getMonth()} ${getYear()}.`}</span>
     </p>
   )
 }
